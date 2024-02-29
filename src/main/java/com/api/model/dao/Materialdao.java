23f648b4bbec950b7cff7316df9079a1db150729
Juliocesarab <161273769+Juliocesarab@users.noreply.github.com>
2024-02-28 22:45:58 -0500

    Delete src directory

--- src/main/java/com/api/model/dao/Materialdao.java
+++
@@ -1,7 +0,0 @@
-package com.api.model.dao;
-
-import com.api.model.Entity.Material;
-import org.springframework.data.repository.CrudRepository;
-
-public interface Clientedao extends CrudRepository<Material, Integer> {
-}
